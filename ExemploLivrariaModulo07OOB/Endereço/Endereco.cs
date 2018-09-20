1f50d1289f9c7aafa597cc90af1e90e758140728
Lucasedu191 <lucasatletico@hotmail.com>
2018-09-20 00:15:03 -0300

    Criaçao do projeto sobre uma livraria

---
+++ ExemploLivrariaModulo07OOB/Endereço/Endereco.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploLivrariaModulo07OOB.Endereco
+{
+    public class Endereco
+    {
+        public int Numero { get; set; }
+        public string Rua { get; set; }
+
+    }
+}
---
+++ ExemploLivrariaModulo07OOB/EnumTipoPessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploLivrariaModulo07OOB
+{
+    public enum EnumTipoPessoa
+    {
+        Cliente = 1,
+        Fornecedor = 2,
+        Autor = 3,
+
+            
+    }
+}
